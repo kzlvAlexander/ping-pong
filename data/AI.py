44fc43dc2a5d31f3cc2ea4a1d7add86400cc31d5
kzlvAlexander <47183774+kzlvAlexander@users.noreply.github.com>
2019-04-25 01:23:40 +0300

    Final with presentation

--- data/AI.py
+++ data/AI.py
@@ -1,5 +1,5 @@
-
 import pygame as pg
+
 
 class AIPaddle:
     def __init__(self, screen_rect, ball_rect):
@@ -9,17 +9,17 @@ class AIPaddle:
         self.move_up = False
         self.move_down = False
         self.screen_response_area_rect = self.screen_rect
-        
+
         if self.difficulty == 'hard':
             num = 1
         elif self.difficulty == 'medium':
             num = 2
         elif self.difficulty == 'easy':
             num = 3
-            
+
         surf = pg.Surface([self.screen_rect.width / num, self.screen_rect.height])
         self.screen_response_area_rect = surf.get_rect()
-        
+
     def update(self, ball_rect, ball, paddle_rect):
         if self.screen_response_area_rect.colliderect(ball_rect):
             if ball_rect.centery < paddle_rect.centery:
@@ -28,7 +28,7 @@ class AIPaddle:
             elif ball_rect.centery > paddle_rect.centery:
                 if not ball.moving_away_from_AI:
                     self.move_down = True
-            
+
     def reset(self):
         self.move_up = False
         self.move_down = False
